2face934bb333eb5fb2801171c23f1f38c76f4ea
cdelv <cdelv@unal.edu.co>
2023-10-24 13:36:21 -0500

    Fix automatic posts

--- assets/js/load_posts.js
+++ assets/js/load_posts.js
@@ -1,96 +1,68 @@
-// Function to populate the latest news section with data
-function populateLatestNews(posts, currentPage, postsPerPage) {
-    const latestNewsContent = document.getElementById("latest-news-content");
-    latestNewsContent.innerHTML = "";
+$(document).ready(function() {
+    // Load the JSON file
+    $.getJSON('pages/post_info.json', function(data) {
+        // Sort posts by date in descending order
+        data.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-    const startIndex = (currentPage - 1) * postsPerPage;
-    const endIndex = startIndex + postsPerPage;
-    const currentPosts = posts.slice(startIndex, endIndex);
+        // Define the number of posts per page
+        const postsPerPage = 6;
+        let currentPage = 1;
 
-    currentPosts.forEach((post) => {
-        const postHTML = `
-            <div class="col-lg-4 col-md-6">
-                <div class="single-latest-news">
-                    <a href="${post.link}">
-                        <div class="latest-news-bg news-bg-${post.newsBg}"></div>
-                    </a>
-                    <div class="news-text-box">
-                        <h3><a href="${post.link}">${post.title}</a></h3>
-                        <p class="blog-meta">
-                            <span class="author"><i class="fas fa-user"></i> ${post.author}</span>
-                            <span class="date"><i class="fas fa-calendar"></i> ${post.date}</span>
-                        </p>
-                        <p class="excerpt">${post.excerpt}</p>
-                        <a href="${post.link}" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-                    </div>
-                </div>
+        // Function to display posts for the current page
+        function displayPosts() {
+            const startIndex = (currentPage - 1) * postsPerPage;
+            const endIndex = startIndex + postsPerPage;
+            const paginatedPosts = data.slice(startIndex, endIndex);
+
+            const $latestNewsContent = $('#latest-news-content');
+            $latestNewsContent.empty();
+
+            paginatedPosts.forEach((post) => {
+                const postHTML = `
+          <div class="col-lg-4 col-md-6">
+            <div class="single-latest-news">
+              <a href="${post.link}">
+                <div class="latest-news-bg news-bg-1"></div>
+              </a>
+              <div class="news-text-box">
+                <h3><a href="${post.link}">${post.title}</a></h3>
+                <p class="blog-meta">
+                  <span class="author"><i class="fas fa-user"></i> ${post.author}</span>
+                  <span class="date"><i class="fas fa-calendar"></i> ${post.date}</span>
+                </p>
+                <p class="excerpt">${post.excerpt}</p>
+                <a href="${post.link}" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
+              </div>
             </div>
+          </div>
         `;
-
-        latestNewsContent.innerHTML += postHTML;
-    });
-}
-
-// Function to create pagination links
-function createPagination(posts, postsPerPage) {
-    const pagination = document.getElementById("pagination");
-    pagination.innerHTML = "";
-
-    const pageCount = Math.ceil(posts.length / postsPerPage);
-
-    for (let i = 1; i <= pageCount; i++) {
-        const listItem = document.createElement("li");
-        const link = document.createElement("a");
-        link.textContent = i;
-        link.href = "#"; // You can add click event listeners to these links
-        listItem.appendChild(link);
-        pagination.appendChild(listItem);
-    }
-}
-
-var posts = [];
-
-// Example data structure for posts (replace this with your JSON data)
-function fetchPostsFromJSON() {
-    fetch("pages/post_info.json") // Replace with the correct path to your JSON file
-        .then((response) => response.json())
-        .then((data) => {
-            const posts = data; // Assuming the JSON structure matches your provided format
-
-            const postsPerPage = 6;
-            let currentPage = 1;
-
-            // Initial population and pagination creation
-            populateLatestNews(posts, currentPage, postsPerPage);
-            createPagination(posts, postsPerPage);
-
-            // Event listener for pagination (you can use a library like jQuery for better handling)
-            document.getElementById("pagination").addEventListener("click", (event) => {
-                if (event.target.tagName === "A") {
-                    currentPage = parseInt(event.target.textContent);
-                    populateLatestNews(posts, currentPage, postsPerPage);
-                }
+                $latestNewsContent.append(postHTML);
             });
-        })
-        .catch((error) => {
-            console.error("Failed to fetch posts:", error);
-        });
-}
+        }
 
-// Fetch and populate posts when the page loads
-fetchPostsFromJSON();
+        // Function to update pagination links
+        function updatePagination() {
+            const $pagination = $('#pagination');
+            $pagination.empty();
 
-const postsPerPage = 6;
-let currentPage = 1;
+            const totalPages = Math.ceil(data.length / postsPerPage);
+            for (let i = 1; i <= totalPages; i++) {
+                const isActive = i === currentPage ? 'active' : '';
+                const paginationHTML = `<li class="page-item ${isActive}"><a class="page-link" href="#">${i}</a></li>`;
+                $pagination.append(paginationHTML);
+            }
 
-// Initial population and pagination creation
-populateLatestNews(posts, currentPage, postsPerPage);
-createPagination(posts, postsPerPage);
+            // Add click event for pagination links
+            $pagination.find('a').on('click', function(e) {
+                e.preventDefault();
+                currentPage = parseInt($(this).text());
+                displayPosts();
+                updatePagination();
+            });
+        }
 
-// Event listener for pagination (you can use a library like jQuery for better handling)
-document.getElementById("pagination").addEventListener("click", (event) => {
-    if (event.target.tagName === "A") {
-        currentPage = parseInt(event.target.textContent);
-        populateLatestNews(posts, currentPage, postsPerPage);
-    }
+        // Initialize the display and pagination
+        displayPosts();
+        updatePagination();
+    });
 });
